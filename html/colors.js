f2c10d91b8c6d8eaa6e53eec6ff2ae23d0ab16bf
wold21 <wold2180@gmail.com>
2021-04-07 00:37:34 +0900

    plus jquery

--- html/colors.js
+++ html/colors.js
@@ -1,19 +1,24 @@
 var Body = {
   setColor: function (color) {
-    document.querySelector("body").style.color = color;
+    // document.querySelector("body").style.color = color;
+    $("body").css("color", color);
   },
   setBackgroundColor: function (color) {
-    document.querySelector("body").style.backgroundColor = color;
+    $("body").css("backgroundColor", color);
   },
 };
 var Links = {
   setColor: function (color) {
-    var aList = document.querySelectorAll("a");
-    var i = 0;
-    while (i < aList.length) {
-      aList[i].style.color = color;
-      i++;
-    }
+    // var aList = document.querySelectorAll("a");
+    // var i = 0;
+    // while (i < aList.length) {
+    //   aList[i].style.color = color;
+    //   i++;
+    // }
+
+    // jquery
+    // 이 웹페이지에 있는 a태그를 제이쿼리로 컨트롤하겠다.
+    $("a").css("color", color);
   },
 };
 
@@ -21,13 +26,13 @@ function nightDayHandler(self) {
   var target = document.querySelector("body");
   if (self.value === "Night") {
     Body.setBackgroundColor("black");
-    Body.setColor("white");
+    Body.setColor("powderblue");
     self.value = "Day";
     Links.setColor("powderblue");
   } else {
     Body.setBackgroundColor("white");
     Body.setColor("black");
     self.value = "Night";
-    Links.setColor("blue");
+    Links.setColor("black");
   }
 }
